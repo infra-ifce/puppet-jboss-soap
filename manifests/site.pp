71e37f99a90a2d76d9d70426eb11c70c49521a33
Romain Pelisse <belaran@gmail.com>
2014-07-08 12:29:27 +0200

    impl. JBoss AS config

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,13 @@
-node 'vm-puppet' {
-  notice("hello $fqdn")
+node /vm-jboss*/ {
+  notice("Setting host $fqdn using Puppet ($puppetversion).")
+  
+  $profile_name='ifce'
+
+  $app_java_opts='-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dsun.lang.ClassLoader.allowArraySyntax=true -XX:+UseLargePages -XX:LargePageSizeInBytes=4m'
+  jboss-as::base { 'jboss-soap': 
+    jvm_memory => '1024',
+  } 
+# conf ifce-tek
+  jboss-as::os_tuning { 'soap-tuning': }
+
 }
---
+++ modules/jboss-as/manifests/init.pp
@@ -0,0 +1,54 @@
+define jboss-as::base($product_name='jboss-soap', $product_home='/usr/share/jboss-soap/jboss-as/', $profile='production', $jvm_memory='2048') {
+
+  $perm_size='256'
+  $user='jboss'
+  $group=$user
+  $java_library_path="$product_home/jboss-ep-5.2/native/lib64"
+  $jboss_user='admin'
+  $jboss_user_password='admin'
+  
+  package { $product_name:
+    ensure => installed,
+  }
+
+  service  { $product_name:
+    ensure => 'running',
+    enable => true,
+    require => File['/usr/share/jboss-soap/jboss-as/server/production/log']
+  }
+
+  file { '/usr/share/jboss-soap/jboss-as/server/production/log':
+    ensure => directory,
+    owner => $user,
+    group => $group,
+    require =>  Package[$product_name]
+  }
+
+  file { "$product_home/server/$profile/run.conf":
+    content => template("jboss-as/run.conf.erb"),
+    owner => $user,
+    group => $group,
+    require => Package[$product_name],
+    notify => Service[$product_name]
+  }
+
+  file { "$product_home/server/$profile/conf/props/soa-users.properties":
+    content => template("jboss-as/soa-users.properties.erb"),
+    owner => $user,
+    group => $group,
+    require => Package[$product_name],
+  }
+
+  file { "$product_home/server/$profile_name":
+    ensure => link,
+    target => "$product_home/server/$profile",
+    require => Package[$product_name]
+  }
+}
+
+define jboss-as::os_tuning($kernel_shmmax='4294967296', $kernel_shmall='1048576', $vm_hugetlb_shm_group='500',$vm_nr_hugepages='1536') {
+  sysctl { 'kernel.shmmax': value => $kernel_shmmax }
+  sysctl { 'kernel.shmall': value => $kernel_shmall }
+  sysctl { 'vm.hugetlb_shm_group': value => $vm_hugetlb_shm_group }
+  sysctl { 'vm.nr_hugepages': value => $kernel_shmall }
+}
