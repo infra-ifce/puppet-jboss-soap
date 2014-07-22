df2a4b13e6008654b40fb57978e418b23da861df
Romain Pelisse <belaran@gmail.com>
2014-07-22 18:13:21 +0200

    Add mod_cluster for httpd

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,19 +6,13 @@ node 'template-jboss' {
   $product_home='/usr/share/jboss-soap/jboss-as/'
   $profile='production'
 
-  $app_java_opts='-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dsun.lang.ClassLoader.allowArraySyntax=true -XX:+UseLargePages -XX:LargePageSizeInBytes=4m'
-  jboss-as::base { 'jboss-soap': 
-    jvm_memory => '1024',
-    proxy_list => "$fqdn:6666",
-  } 
-# conf ifce-tek
-  jboss-as::os_tuning { 'soap-tuning': }
-
+#  FIXME: to finish
 #  httpd::mod_cluster_client { 'mod_cluster_libs':
 #    deploy_folder => "$product_home/server/$profile/deploy",
 #    require => Jboss-As::Os_Tuning['soap-tuning'],
 #  }
-
+  jboss-as::os_tuning { 'soap-tuning': } 
+  
   httpd::mod_cluster { 'conf_mod_cluster_jboss':
     jboss_server_xml => "$product_home/server/$profile/deploy/jbossweb.sar/server.xml", 
     notify => Service['jboss-soap']
@@ -27,8 +21,30 @@ node 'template-jboss' {
 
 node template-frontlb { }
 
-node /vm-jboss*/ inherits template-jboss {}
+node /vm-jboss*/ inherits template-jboss {
 
-node vm-puppet inherits template-jboss {}
+  $app_java_opts='-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dsun.lang.ClassLoader.allowArraySyntax=true -XX:+UseLargePages -XX:LargePageSizeInBytes=4m'
+  jboss-as::base { 'jboss-soap': 
+    jvm_memory => '2048',
+    proxy_list => "vm-frontlb.ifce.lan:6666",
+  } 
+}
 
-node /vm-frontlb*/ inherits template-frontlb {}
+node /vm-frontlb*/ inherits template-frontlb {
+   
+  httpd::base { 'httpd-setup': } 
+  httpd::mod_cluster_conf { 'mod-cluster-conf': } 
+}
+
+node vm-puppet inherits template-jboss {
+ 
+  # vm-frontlb conf
+  httpd::base { 'httpd-setup': } 
+  httpd::mod_cluster_conf { 'mod-cluster-conf': } 
+
+  # vm-jboss conf
+  $app_java_opts='-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dsun.lang.ClassLoader.allowArraySyntax=true -XX:+UseLargePages -XX:LargePageSizeInBytes=4m'
+  jboss-as::base { 'jboss-soap': 
+    jvm_memory => '1024',
+  }
+}
--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,11 +1,10 @@
-define httpd::base($jboss_server_xml, $user='jboss', $group='jboss') {
+define httpd::base($httpd_mod_cluster_packages=['httpd'], $httpd_service_name='httpd') {
 
-  $httpd_mod_cluster_packages=['']
   package { $httpd_mod_cluster_packages:
     ensure => installed,
   }
 
-  service { 'httpd':
+  service { $httpd_service_name:
     enable => true,
     ensure => running,
     require => Package[$httpd_mod_cluster_packages]
@@ -21,4 +20,12 @@ define httpd::mod_cluster($jboss_server_xml, $user='jboss', $group='jboss') {
   }
 }
 
+define httpd::mod_cluster_conf($httpd_conf='/etc/httpd/conf.d', $modcluster_cache='/var/cache/mod_cluster', $modcluster_port='6666', $modcluster_bind_address='*', $httpd_mod_cluster_packages=['httpd'], $httpd_service_name='httpd') {
 
+  file { "$httpd_conf/mod_cluster.conf":
+    content => template("httpd/mod_cluster.conf.erb"),
+    owner => 'root', group => 'root',
+    require => Package[$httpd_mod_cluster_packages],
+    notify => Service[$httpd_service_name]
+  }
+}
