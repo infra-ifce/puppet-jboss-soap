6b2967f7223cdd6a7c616cf086cb9dbdfea9ab5b
ananot <an@ifce.fr>
2014-09-04 17:14:03 +0200

    fichiers config tomcat7

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -1,14 +1,18 @@
-define tomcat7::setup($catalina_base='/usr/share/tomcat7', $packages=[ 'java-1.7.0-openjdk', 'tomcat7', 'tomcat7-admin-webapps', 'tomcat7-log4j', 'tomcat-native']) {
+define tomcat7::setup($catalina_base='/usr/share/tomcat7', $packages=[ 'java-1.7.0-openjdk', 'tomcat7', 'tomcat7-admin-webapps', 'tomcat7-log4j', 'tomcat-native']) 
+{
+  $jvm_memory='1792'
+  $perm_size='512'
+  $tomcat_user='tomcat'
 
   package { $packages:
     ensure => installed,
   }
 
-    service { 'tomcat7':
-      ensure => running,
-      enable => true,
-      require => [ File['/etc/tomcat7/tomcat7.conf'], Package['tomcat7'] ]
-    }
+  service { 'tomcat7':
+    ensure => running,
+    enable => true,
+    require => [ File['/etc/tomcat7/tomcat7.conf'], Package['tomcat7'] ]
+  }
   
   file { '/usr/sbin/tomcat7':
     content => template("tomcat7/tomcat7.erb"),
@@ -16,5 +20,20 @@ define tomcat7::setup($catalina_base='/usr/share/tomcat7', $packages=[ 'java-1.7
     require => Package[$packages],
   }  
 
+  file { '/etc/tomcat7/tomcat7.conf':
+    content => template("tomcat7/tomcat7.conf.erb"),
+    mode => 664,
+    owner => $tomcat_user,
+    group => $tomcat_user,
+    require => Package[$packages],
+  } 
+
+  file { '/etc/tomcat7/tomcat-users.xml':
+      source => 'puppet:///modules/tomcat7/tomcat-users.xml',
+      mode => 664, 
+      owner => $tomcat_user,
+      group => $tomcat_user,
+      notify => Service['tomcat7'],
+  }
   
 }
