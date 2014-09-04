281df2d2ad0d29c65c40b6494a198c2e66ed2d23
ananot <an@ifce.fr>
2014-09-04 15:30:11 +0200

    modif droits sh tomcat7

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -6,7 +6,7 @@ define tomcat7::setup($catalina_base='/usr/share/tomcat7', $packages=[ 'java-1.7
   
   file { '/usr/sbin/tomcat7':
     content => template("tomcat7/tomcat7.erb"),
-    mode => 744,
+    mode => 755,
     require => Package[$packages],
   }  
 
