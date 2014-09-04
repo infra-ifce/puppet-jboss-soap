204582bd4b41de4bc201398eba1924f463e4537e
ananot <an@ifce.fr>
2014-09-04 16:39:03 +0200

    autorun tomcat7

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -3,6 +3,12 @@ define tomcat7::setup($catalina_base='/usr/share/tomcat7', $packages=[ 'java-1.7
   package { $packages:
     ensure => installed,
   }
+
+    service { 'tomcat7':
+      ensure => running,
+      enable => true,
+      require => [ File['/etc/tomcat7/tomcat7.conf'], Package['tomcat7'] ]
+    }
   
   file { '/usr/sbin/tomcat7':
     content => template("tomcat7/tomcat7.erb"),
